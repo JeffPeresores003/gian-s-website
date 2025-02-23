295c3afd8147d7ea1c9cd5b1f6a129a1c79089b9
JeffPeresores003 <jeffrey.peresores@bisu.edu.ph>
2025-02-23 23:11:25 +0800

    commit

--- giansfoodhouse/src/Components/Header.js
+++ giansfoodhouse/src/Components/Header.js
@@ -2,22 +2,30 @@ import React from 'react';
 
 function Header({ setIsAuthModalOpen, setIsLogin }) {
   return (
-    <header>
-      <h1>Gian's Foodhouse</h1>
-      <nav>
-        <button onClick={() => {
-          setIsLogin(true);
-          setIsAuthModalOpen(true);
-        }}>
-          Login
-        </button>
-        <button onClick={() => {
-          setIsLogin(false);
-          setIsAuthModalOpen(true);
-        }}>
-          Sign Up
-        </button>
-      </nav>
+    <header className="header">
+      <div className="header-container">
+        <h1 className="logo">Gian's Foodhouse</h1>
+        <nav className="nav">
+          <button
+            className="nav-button login-button"
+            onClick={() => {
+              setIsLogin(true);
+              setIsAuthModalOpen(true);
+            }}
+          >
+            Login
+          </button>
+          <button
+            className="nav-button signup-button"
+            onClick={() => {
+              setIsLogin(false);
+              setIsAuthModalOpen(true);
+            }}
+          >
+            Sign Up
+          </button>
+        </nav>
+      </div>
     </header>
   );
 }
